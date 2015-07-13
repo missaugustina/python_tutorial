82fe89523c19b68a730be75c155182fa2c06fc64
Augustina Ragwitz <auggy@cpan.org>
2015-07-13 14:35:19 -0700

    Add exceptions

--- hello.py
+++ hello.py
@@ -3,10 +3,17 @@
 import sys # includes operating system functionality
 
 def Hello(name):
+  if not isinstance(name, str):
+    raise NameError('No name provided')
+
   return 'Hello ' + name + '!!!'
 
 def main ():
-  print Hello(sys.argv[1])
+  args = sys.argv
+  if len(args) < 2:
+    raise IndexError('Please provide a name!')
+  name = args[1] # set to None to trigger NameError exception in Hello()
+  print Hello(name)
 
 if __name__ == '__main__':
   main()
